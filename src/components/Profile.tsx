80895da31da3a9cc615079164b952001928abd05
theblaze98 <dionijvargas@gmail.com>
2024-02-24 18:29:08 -0400

    Seo and accesibility update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,33 +3,33 @@ import profileImg from '@/assets/profile.jpg'
 
 export default function Profile() {
 	return (
-		<section className='flex flex-col w-1/3 gap-3 items-center max-md:w-full max-md:flex-row max-md:items-center max-md:justify-center'>
-			<figure className='rounded-full overflow-hidden w-2/3 border-2 border-light-accent-100 dark:border-dark-accent-100 p-[2px] max-md:w-1/4'>
+		<section className='flex flex-col w-1/3 items-center max-md:w-full max-md:flex-row max-md:justify-center max-md:items-center'>
+			<figure className='rounded-full overflow-hidden w-40 border-2 border-light-accent-100 dark:border-dark-accent-100 p-[2px] max-md:w-1/4'>
 				<img
 					src={profileImg}
 					alt='profile'
 					className='rounded-full'
 				/>
 			</figure>
-			<div>
-				<h1 className='mt-3 text-xl font-medium self-center'>
+			<div className='flex flex-col gap-3 mt-5'>
+				<h1 className='text-2xl font-medium'>
 					Dionicio Vargas
 				</h1>
 				<a
 					href='https://github.com/theblaze98'
 					target='_blank'
-					className='flex gap-2 items-center text-light-text-200 dark:text-dark-text-200 text-sm ml-4'>
+					className='flex gap-2 items-center text-light-text-200 dark:text-dark-text-200 text-md ml-4'>
 					<FaGithub /> theblaze98
 				</a>
 				<a
 					href='https://linkedin.com/in/dionicio-vargas-832821237/'
 					target='_blank'
-					className='flex gap-2 items-center text-light-text-200 dark:text-dark-text-200 text-sm ml-4'>
+					className='flex gap-2 items-center text-light-text-200 dark:text-dark-text-200 text-md ml-4'>
 					<FaLinkedin /> Dionicio Vargas
 				</a>
 				<a
 					href='mailto:dioniciovargas1226@gmail.com?Subject=Hola%20Dionicio%20%F0%9F%91%8B'
-					className='flex gap-2 items-center text-light-text-200 dark:text-dark-text-200 text-xs ml-4'>
+					className='flex gap-2 items-center text-light-text-200 dark:text-dark-text-200 text-sm ml-4'>
 					<FaEnvelope /> dioniciovargas1226@gamil.com
 				</a>
 			</div>
